e080519198957d5b0358a618512eb9f97263bda0
Ashok Murthy <ashokmurthy88@gmail.com>
2021-05-31 23:59:09 +0900

    python application to conect to mysql database

---
+++ app_db.py
@@ -0,0 +1,22 @@
+import mysql.connector
+
+con = mysql.connector.connect(
+user = "ardit700_student",
+password = "ardit700_student",
+host = "108.167.140.122",
+database = "ardit700_pm1database",
+)
+
+cursor = con.cursor()
+
+word = input("Enter the word: ")
+
+query = cursor.execute("SELECT * FROM Dictionary where Expression = '%s' " % word)
+result = cursor.fetchall()
+
+# result may be empty (False)
+if result:
+    for item in result:
+        print(item[1])
+else:
+    print("no word found!")
