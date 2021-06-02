9be343b87b1eb0e8e7f14db76216964fd9cc74fd
Ashok Murthy <ashokmurthy88@gmail.com>
2021-06-02 20:40:31 +0900

    added population details in web map

--- volcano_app.py
+++ volcano_app.py
@@ -26,12 +26,22 @@ def color_producer(elevation):
         return "red"
 
 map = folium.Map(location=[48.7767982,-121.8109970], zoom_start=7)
-fg = folium.FeatureGroup(name="My Group")
+fgv = folium.FeatureGroup(name="Volcanoes")
 
 for lt, ln, el, name in zip(lat, lan, elev, name):
     iframe = folium.IFrame(html=html_link % (name, name, el), width=200, height=100)
     #fg.add_child(folium.Marker(location = [lt, ln], popup=folium.Popup(iframe), icon=folium.Icon(color=color_producer(el))))
-    fg.add_child(folium.CircleMarker(location = [lt, ln], radius=7, popup=folium.Popup(iframe), fill_color=color_producer(el), color='grey', fill_opacity=0.6))
+    fgv.add_child(folium.CircleMarker(location = [lt, ln], radius=7, popup=folium.Popup(iframe), fill_color=color_producer(el), color='grey', fill_opacity=0.6))
 
-map.add_child(fg)
+fgp = folium.FeatureGroup(name="Population")
+
+fgp.add_child(folium.GeoJson(
+    data=open("resources/webmap/world_1.json", "r", encoding="utf-8-sig").read(), 
+    style_function=lambda x : {'fillColor':'green' if x['properties']['POP2005'] < 10000000 else 'orange'
+    if 10000000 <= x['properties']['POP2005'] < 20000000 else 'red'}))
+
+map.add_child(fgv)
+map.add_child(fgp)
+map.add_child(folium.LayerControl())
+
 map.save("Map1.html")
